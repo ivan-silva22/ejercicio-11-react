5b9031c715c00bbe67cd1af81bbc904154da2005
Ivan Silva <silvaivan280@gmail.com>
2023-06-02 09:46:42 -0300

    agregue el componente Titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Titulo from './Titulo';
 
 function App() {
   
 
   return (
-    <>
+    <section>
+      <Titulo/>
       
-    </>
+    </section>
   )
 }
 
---
+++ src/Titulo.jsx
@@ -0,0 +1,10 @@
+const Titulo = () => {
+    return (
+        <section className="text-center">
+            <h1 className="display-4">Noticias</h1>
+            <hr />
+        </section>
+    );
+};
+
+export default Titulo;
